fd5e434d5204d605001f0044c418b0c6507e086b
carlosjrcabello <carlosjrcabello@gmail.com>
2011-10-05 16:16:45 -0300

    Commit local.

---
+++ LoadObject/src/com/wp/carlos4web/vraptor/LoadObject.java
@@ -0,0 +1,10 @@
+package com.wp.carlos4web.vraptor;
+
+@java.lang.annotation.Target(value={java.lang.annotation.ElementType.PARAMETER})
+@java.lang.annotation.Retention(value=java.lang.annotation.RetentionPolicy.RUNTIME)
+public @interface LoadObject
+{
+	boolean required() default false;
+	
+	String redirectToWhenObjectNotFound() default "";
+}
